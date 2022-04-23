acee4a95f04e52e48beb8bec1748fa5ea8e806f5
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-23 17:25:04 +0300

    2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,11 +1,11 @@
 import Player from '@vimeo/player';
 
+const iFreim = document.querySelector("#vimeo-player");
+
+console.log(iFreim)
+
+
 const player = new Player('handstick', {
-    id: "vimeo-player",
-    width: 640
+    iFreim,
 });
 
-player.on('play', function() {
-    console.log('played the video!');
-    console.log(player)
-});
