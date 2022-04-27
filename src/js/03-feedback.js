0356be6bc3af7bad2ce500cab1f4e6ae2e486c41
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-27 21:47:47 +0300

    finished dz8

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const refs = {
+    form: document.querySelector('.feedback-form'),
+    emailInput : document.querySelector('[name="email"]'),
+    messageInput :document.querySelector('[name="message"]'),
+}
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onInput,500));
+
+populateText();
+
+const formData = {
+    email: '',
+    message:'',
+};  
+  
+function onInput(evt) {
+    formData[evt.target.name] = evt.target.value;
+
+  localStorage.setItem("feedback-form-state", JSON.stringify(formData)); 
+};
+
+function populateText(){
+    const getMessage = localStorage.getItem("feedback-form-state");
+
+    if (getMessage) {
+        const getObject = JSON.parse(getMessage);
+    
+        refs.emailInput.value = getObject.email; 
+        refs.messageInput.value = getObject.message;
+    }
+}
+
+function onFormSubmit(evt) {
+    evt.preventDefault();
+    const rezult = localStorage.getItem("feedback-form-state");
+    console.log(JSON.parse(rezult) )
+    evt.currentTarget.reset();
+    localStorage.removeItem("feedback-form-state");
+};
